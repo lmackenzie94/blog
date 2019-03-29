ee8f5ca6d9fd52653e3c7087986c67387598264c
arctiqjacob <jacob.mammoliti@arctiq.ca>
2019-03-29 15:16:45 -0400

    updated readme and added login, register code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # blog
+## Set Up Environment
+
+```
+~]$ pip install flask
+~]$ export FLASK_APP=main.py
+~]$ flask run
+   * Running on http://127.0.0.1:5000/
+```
+
--- main.py
+++ main.py
@@ -1,8 +1,51 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+user = {'firstname': 'admin',
+        'lastname': 'user',
+        'username': 'admin',
+        'password': 'admin123'}
+
+def valid_login(username, password):
+    if username == user['username'] and password == user['password']:
+        return True
+    
+    return False
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    error = None
+
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_user_in(request.form['username'])
+        else:
+            error = "Invalid username/password."
+
+    return render_template('index.html', error=error)
+
+@app.route('/dashboard', methods=['POST', 'GET'])
+def dashboard():
+    error = None
+
+    return render_template('dashboard.html', error=error)
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    error = None
+
+    if request.method == 'POST':
+         if valid_register(request.form['username'],
+                           request.form['password'],
+                           request.form['repeat_password'],
+                           request.form['firstname'],
+                           request.form['lastname']):
+             return log_user_in(request.form['username'])
+         else:
+             error = 'Invalid register. Figure it out.'
+
+    return render_template('register.html', error=error)
