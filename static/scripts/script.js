28a1b13c10109d364c146f9dc6afa8bea19438b1
Luke <mackenzieluke94@gmail.com>
2019-03-29 21:04:35 -0400

    added sign up form

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,8 +1,17 @@
-const input = document.getElementById('password');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+const match = document.querySelector('.passwordMatch');
 const checkbox = document.getElementById('togglePassword');
 
 const togglePassword = () => {
-    input.type === "password" ? input.type = "text" : input.type = "password";
+    password.type === "password" ? password.type = "text" : password.type = "password";
+    confirmPassword.type === "password" ? confirmPassword.type = "text" : confirmPassword.type = "password";
 }
 
+const passwordMatch = () => {
+    password.value === confirmPassword.value ? (match.innerHTML = '<i class="fas fa-check-circle"></i>') : (match.innerHTML = '<i class="fas fa-times-circle"></i>');
+}
+
+password.addEventListener('change', passwordMatch);
+confirmPassword.addEventListener('change', passwordMatch);
 checkbox.addEventListener('click', togglePassword);
