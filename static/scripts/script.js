e3bb41bc894b03e2e5d5c47bed115c4c60a35b81
Luke <mackenzieluke94@gmail.com>
2019-03-31 17:52:31 -0400

    fixed JS error

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -4,14 +4,23 @@ const match = document.querySelector('.passwordMatch');
 const checkbox = document.getElementById('togglePassword');
 
 const togglePassword = () => {
-    password.type === "password" ? password.type = "text" : password.type = "password";
-    confirmPassword.type === "password" ? confirmPassword.type = "text" : confirmPassword.type = "password";
+    if (confirmPassword === null) {
+        password.type === "password" ? password.type = "text" : password.type = "password";
+    } else {
+        password.type === "password" ? password.type = "text" : password.type = "password";
+        confirmPassword.type === "password" ? confirmPassword.type = "text" : confirmPassword.type = "password";
+    }
 }
 
 const passwordMatch = () => {
+    if (confirmPassword === null) return;
     password.value === confirmPassword.value ? (match.innerHTML = '<i class="fas fa-check-circle"></i>') : (match.innerHTML = '<i class="fas fa-times-circle"></i>');
 }
 
 password.addEventListener('change', passwordMatch);
-confirmPassword.addEventListener('change', passwordMatch);
+
+if (confirmPassword !== null) {
+    confirmPassword.addEventListener('change', passwordMatch);
+}
+
 checkbox.addEventListener('click', togglePassword);
